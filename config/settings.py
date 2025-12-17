a764bd398a62aed711af7185742cd5879bfa1a06
s1f102302922 <s1f102302922@iniad.org>
2025-12-17 14:48:57 +0900

    views.pyの修正

--- config/settings.py
+++ config/settings.py
@@ -117,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'project' / 'static',#追加事項
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
